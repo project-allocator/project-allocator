6edc4e93b6a66739a0b3ba5ce4576aeb167e7977
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-08-02 15:08:23 +0100

    Fix: Add sslmode=require parameter to the DATABASE_URL in production

--- src/db.py
+++ src/db.py
@@ -7,15 +7,17 @@ from . import models
 
 # Load database url from environment
 load_dotenv()
+FASTAPI_ENV = os.environ.get("FASTAPI_ENV")
 DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 DATABASE_DOMAIN = os.environ.get("DATABASE_DOMAIN")
-FASTAPI_ENV = os.environ.get("FASTAPI_ENV")
+DATABASE_NAME = "default"
 
 # Azure PostgreSQL requires the username to be in the format of 'username@domain'
 if FASTAPI_ENV == "production":
     DATABASE_USERNAME = f"{DATABASE_USERNAME}@{DATABASE_DOMAIN}"
+    DATABASE_NAME += "?sslmode=require"
 
-DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_DOMAIN}:5432/default"
+DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_DOMAIN}:5432/{DATABASE_NAME}"
 
 engine = create_engine(DATABASE_URL)
