c9f5550d84e7e9d28e74bb9186db720682f56e46
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-08-03 19:22:48 +0100

    Fix: Make SSL optional for database connection

--- src/db.py
+++ src/db.py
@@ -11,13 +11,12 @@ FASTAPI_ENV = os.environ.get("FASTAPI_ENV")
 DATABASE_USERNAME = os.environ.get("DATABASE_USERNAME")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 DATABASE_DOMAIN = os.environ.get("DATABASE_DOMAIN")
-DATABASE_NAME = "default"
+DATABASE_SSL = os.environ.get("DATABASE_SSL")
 
 # Azure PostgreSQL requires the username to be in the format of 'username@domain'
 if FASTAPI_ENV == "production":
     DATABASE_USERNAME = f"{DATABASE_USERNAME}@{DATABASE_DOMAIN}"
-    DATABASE_NAME += "?sslmode=require"
 
-DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_DOMAIN}:5432/{DATABASE_NAME}"
+DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_DOMAIN}:5432/default{'?sslmode=require' if DATABASE_SSL == 'true' else ''}"
 
 engine = create_engine(DATABASE_URL)
