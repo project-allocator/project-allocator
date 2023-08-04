465176dcf5b4f8f66a5223660bbbe1ccf85d0125
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-08-04 19:04:26 +0100

    Feat: Rewrite setup.sh to auto-generate PostgreSQL credentials

--- setup.sh
+++ setup.sh
@@ -76,6 +76,7 @@ function setup_repo() {
   
   echo "Please make sure you have met the following requirements:"
   echo "* You have installed the 'jq' command."
+  echo "* You have installed the 'pwgen' command."
   echo "* You have installed and logged in to the 'gh' GitHub CLI command."
   echo "* You have installed and logged in to the 'wf' Wayfinder CLI command."
   echo "* You have installed the 'kubctl' command."
@@ -90,6 +91,10 @@ function setup_repo() {
   echo "Checking requirements..."
   if ! command -v jq > /dev/null; then
     echo "Error: The 'jq' command is not installed."
+    exit 1
+  fi
+  if ! command -v pwgen > /dev/null; then
+    echo "Error: The 'pwgen' command is not installed."
     exit 1
   fi
   if ! command -v gh > /dev/null; then
@@ -170,7 +175,7 @@ function setup_repo() {
 
   # Create a Kubernetes secret so that kubelet can pull our container image.
   echo "Creating secrets on the Kubernetes cluster..."
-  export GITHUB_TOKEN=$github_token
+  export GITHUB_TOKEN="$github_token"
   username="$(gh api user | jq -r '.login')"
   # Get access to the kuberenetes namespace in order to create a Kubernetes secret.
   wf access cluster to1.aks-stdnt1 --role namespace.admin --namespace "$workspace_name"-project-allocator-dev
@@ -178,6 +183,16 @@ function setup_repo() {
   kubectl delete secret ghcr-login-secret --namespace "$workspace_name"-project-allocator-dev > /dev/null 2>&1 || true
   kubectl create secret docker-registry ghcr-login-secret --namespace "$workspace_name"-project-allocator-dev --docker-username="$username" --docker-password="$github_token" --docker-server=ghcr.io
 
+  # Generate and save random database credentials for Azure PostgreSQL.
+  echo "Generating and saving database credentials..."
+  # Server name must be lowercase alphanumeric.
+  server_name="$(pwgen -Ans 16 1)"
+  # Administrator login must be lowercase alphanumeric and cannot start with a number.
+  administrator_login="$(pwgen -As 1 1)$(pwgen -Ans 15 1)"
+  # Administrator password must be alphanumeric and contain uppercase letters.
+  administrator_password="$(pwgen -cns 32 1)"
+  sed "s/<SERVER_NAME>/$server_name/g;s/<ADMINISTRATOR_LOGIN>/$administrator_login/g;s/<ADMINISTRATOR_PASSWORD>/$administrator_password/g" wayfinder/db-template.yaml > wayfinder/db.yaml
+  
   echo "GitHub repository setup complete!"
   return 0
 }
