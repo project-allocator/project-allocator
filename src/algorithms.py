c70939412989fa477c8a2417ef7bd2d9ed268542
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-10-30 21:03:21 +0000

    Feat: Add another variation of allocation algorithm

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,5 @@
 import random
+import math
 from typing import List
 
 from .config import config
@@ -8,6 +9,7 @@ from .models import Project, Shortlist, User
 # You can add custom configuration entries to config.yaml and load them here.
 students_per_project = config["projects"]["allocations"]["students"]
 
+# Randomly allocate students to projects using `students_per_project` given in config.yaml
 # fmt:off
 def allocate_projects_random(
     users: List[User],
@@ -25,6 +27,30 @@ def allocate_projects_random(
         non_allocatees = non_allocatees[students_per_project:]
     return {"ok": True}
 
+
+# Randomly allocate students
+# fmt:off
+def allocate_projects_random_adaptive(
+    users: List[User],
+    projects: List[Project],
+    shortlists: List[Shortlist],
+):
+    # Only allocate students to approved projects
+    projects = list(filter(lambda project: project.approved, projects))
+    non_allocatees = list(filter(lambda user: user.role == "student" and user.allocated == None, users))
+    random.shuffle(non_allocatees)
+    # Find the number of students per project based on the number of projects available.
+    students_per_project = math.ceil(len(non_allocatees) / len(projects))
+    for project in projects:
+        # Reset students allocated previously
+        project.allocatees = []
+        project.allocatees += non_allocatees[:students_per_project]
+        non_allocatees = non_allocatees[students_per_project:]
+    return {"ok": True}
+
+
+# Allocate students by prioritizing students who shortlisted projects with highest preference.
+# Randomly allocate the rest of students if any project has members less than `student_per_project` given in config.yaml
 # fmt:off
 def allocate_projects_shortlist(
     projects: List[Project], 
@@ -57,8 +83,8 @@ def allocate_projects_shortlist(
         student.accepted = None
     return {"ok": True}
 
-
-# raise HTTPException(status_code=401, detail="User is not admin.")
+# Write your own version of allocation code by following the examples above.
+# You can also return HTTP errors by raising exception e.g. `raise HTTPException(status_code=401, detail="Error message here.")`
 def allocate_projects_custom():
     pass
 
