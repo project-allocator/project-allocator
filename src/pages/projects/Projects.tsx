cfa5254b03f8a9259a6face5c898866e90264137
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-11-07 01:01:03 +0000

    Refactor: Remove duplicate navigations to page for adding a project

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,9 +1,7 @@
 import { ProjectRead, ProjectService } from "@/api";
 import { ProjectsTable } from "@/components/ProjectTable";
-import StaffRoute from "@/routes/StaffRoute";
-import { PlusOutlined } from '@ant-design/icons';
-import { Button, Divider, Space, Tooltip, Typography } from "antd";
-import { Link, useLoaderData } from "react-router-dom";
+import { Divider, Typography } from "antd";
+import { useLoaderData } from "react-router-dom";
 
 const { Title } = Typography;
 
@@ -16,18 +14,9 @@ export default function Projects() {
 
   return (
     <>
-      <Space className="flex items-end justify-between">
-        <Title level={3} className="mb-0">
-          List of All Projects
-        </Title>
-        <StaffRoute>
-          <Tooltip title="Add">
-            <Link to="/projects/add" >
-              <Button shape="circle" icon={<PlusOutlined />} />
-            </Link>
-          </Tooltip>
-        </StaffRoute>
-      </Space>
+      <Title level={3} className="mb-0">
+        List of All Projects
+      </Title>
       <Divider />
       <ProjectsTable projects={projects} />
     </>
