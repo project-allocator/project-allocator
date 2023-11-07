39cd7727d54cc5379a38856da46c7c5283804011
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-11-07 01:04:42 +0000

    Refactor: Remove isRouteErrorResponse check

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,37 +1,25 @@
 import { Button, Layout, Result } from 'antd';
 import { ResultStatusType } from "antd/es/result";
-import { isRouteErrorResponse, useNavigate, useRouteError } from "react-router-dom";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function Error() {
   const navigate = useNavigate();
   const error = useRouteError() as any;
+  // TODO: Print the error for debugging purposes.
   console.error(error);
 
   return (
     <Layout className="grid place-content-center">
-      {isRouteErrorResponse(error)
-        ? (
-          <Result
-            status={error.status as ResultStatusType}
-            title={`${error.status} ${error.statusText}`}
-            subTitle={`${error.error?.message}`}
-            extra={
-              <Button type="primary" onClick={() => navigate(-1)}>
-                Go Back
-              </Button>
-            }
-          />
-        ) : (
-          <Result
-            status="500"
-            title="Something went wrong"
-            extra={
-              <Button type="primary" onClick={() => navigate(-1)}>
-                Go Back
-              </Button>
-            }
-          />
-        )}
+      <Result
+        status={error.status as ResultStatusType}
+        title={`${error.status} ${error.statusText}`}
+        subTitle={error.body?.detail}
+        extra={
+          <Button type="primary" onClick={() => navigate(-1)}>
+            Go Back
+          </Button>
+        }
+      />
     </Layout>
   );
 }
