09a82669be8de7cc1745b6ffb4294930227c3709
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-12-23 18:54:13 +0900

    Refactor: Remove unnecessary nullable=False in TimestampMixin

--- src/mixins/timestamp.py
+++ src/mixins/timestamp.py
@@ -4,11 +4,9 @@ from sqlmodel import Field, SQLModel
 
 class TimestampMixin(SQLModel):
     created_at: datetime = Field(
-        nullable=False,
         default_factory=datetime.utcnow,
     )
     updated_at: datetime = Field(
-        nullable=False,
         default_factory=datetime.utcnow,
         sa_column_kwargs={"onupdate": datetime.utcnow},
     )
