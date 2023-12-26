f7e2706243c4f1dc3114308c9849f4b95773446c
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-12-26 18:26:35 +0900

    Feat: Add .editorcofig specifying max line width

--- src/main.tsx
+++ src/main.tsx
@@ -3,51 +3,30 @@ import "./index.css";
 import { MsalProvider } from "@azure/msal-react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  Navigate,
-  Outlet,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { Navigate, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import { msalInstance } from "./auth";
 import CenterLayout from "./components/layouts/CenterLayout";
 import HeaderLayout from "./components/layouts/HeaderLayout";
 import SiderLayout from "./components/layouts/SiderLayout";
 import { ConfigContextProvider } from "./contexts/ConfigContext";
 import { MessageContextProvider } from "./contexts/MessageContext";
+import { TemplateContextProvider } from "./contexts/TemplateContext";
 import { UserContextProvider } from "./contexts/UserContext";
 import Error from "./pages/Error";
 import SignIn from "./pages/SignIn";
-import ManageAllocations, {
-  manageAllocationsLoader,
-} from "./pages/admins/ManageAllocations";
+import ManageAllocations, { manageAllocationsLoader } from "./pages/admins/ManageAllocations";
 import ManageData from "./pages/admins/ManageData";
 import ManageNotifications from "./pages/admins/ManageNotifications";
-import ManageProjects, {
-  manageProjectsLoader,
-} from "./pages/admins/ManageProjects";
+import ManageProjects, { manageProjectsLoader } from "./pages/admins/ManageProjects";
 import ManageUsers, { manageUsersLoader } from "./pages/admins/ManageUsers";
-import AllocatedProject, {
-  allocatedProjectLoader,
-} from "./pages/allocations/AllocatedProject";
+import AllocatedProject, { allocatedProjectLoader } from "./pages/allocations/AllocatedProject";
 import AddProject, { addProjectAction } from "./pages/projects/AddProject";
-import EditProject, {
-  editProjectAction,
-  editProjectLoader,
-} from "./pages/projects/EditProject";
+import EditProject, { editProjectAction, editProjectLoader } from "./pages/projects/EditProject";
 import Project, { projectLoader } from "./pages/projects/Project";
 import Projects, { projectsLoader } from "./pages/projects/Projects";
-import ProposedProject, {
-  proposedProjectLoader,
-} from "./pages/proposals/ProposedProject";
-import ShortlistedProjects, {
-  shortlistedProjectsLoader,
-} from "./pages/shortlists/ShortlistedProjects";
-import {
-  EditUser,
-  editUserAction,
-  editUserLoader,
-} from "./pages/users/EditUser";
+import ProposedProject, { proposedProjectLoader } from "./pages/proposals/ProposedProject";
+import ShortlistedProjects, { shortlistedProjectsLoader } from "./pages/shortlists/ShortlistedProjects";
+import { EditUser, editUserAction, editUserLoader } from "./pages/users/EditUser";
 import User, { userLoader } from "./pages/users/User";
 import AdminRoute from "./routes/AdminRoute";
 import AuthRoute from "./routes/AuthRoute";
@@ -208,12 +187,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
       <ConfigContextProvider>
-        <UserContextProvider>
-          <MessageContextProvider>
-            <RouterProvider router={router} />
-          </MessageContextProvider>
-        </UserContextProvider>
+        <TemplateContextProvider>
+          <UserContextProvider>
+            <MessageContextProvider>
+              <RouterProvider router={router} />
+            </MessageContextProvider>
+          </UserContextProvider>
+        </TemplateContextProvider>
       </ConfigContextProvider>
     </MsalProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
