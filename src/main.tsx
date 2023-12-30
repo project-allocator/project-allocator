f235ee11fae43381ebfcec3f5e8f46a5a458e67e
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-12-30 10:43:23 +0900

    Refactor: Rewrite router using route elements for readability

--- src/main.tsx
+++ src/main.tsx
@@ -2,198 +2,94 @@ import "./index.css";
 
 import { MsalProvider } from "@azure/msal-react";
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { Navigate, Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import { msalInstance } from "./auth";
 import CenterLayout from "./components/layouts/CenterLayout";
-import HeaderLayout from "./components/layouts/HeaderLayout";
 import SiderLayout from "./components/layouts/SiderLayout";
-import { ConfigContextProvider } from "./contexts/ConfigContext";
 import { MessageContextProvider } from "./contexts/MessageContext";
-import { TemplateContextProvider } from "./contexts/TemplateContext";
-import { UserContextProvider } from "./contexts/UserContext";
 import Error from "./pages/Error";
 import SignIn from "./pages/SignIn";
 import ManageAllocations from "./pages/admins/ManageAllocations";
 import ManageData from "./pages/admins/ManageData";
 import ManageNotifications from "./pages/admins/ManageNotifications";
-import ManageProjects, { manageProjectsLoader } from "./pages/admins/ManageProjects";
-import ManageUsers, { manageUsersLoader } from "./pages/admins/ManageUsers";
-import AllocatedProject, { allocatedProjectLoader } from "./pages/allocations/AllocatedProject";
-import AddProject, { addProjectAction } from "./pages/projects/AddProject";
-import EditProject, { editProjectAction, editProjectLoader } from "./pages/projects/EditProject";
-import Project, { projectLoader } from "./pages/projects/Project";
-import Projects, { projectsLoader } from "./pages/projects/Projects";
-import ProposedProject, { proposedProjectLoader } from "./pages/proposals/ProposedProject";
-import ShortlistedProjects, { shortlistedProjectsLoader } from "./pages/shortlists/ShortlistedProjects";
-import { EditUser, editUserAction, editUserLoader } from "./pages/users/EditUser";
-import User, { userLoader } from "./pages/users/User";
+import ManageProjects from "./pages/admins/ManageProjects";
+import ManageUsers from "./pages/admins/ManageUsers";
+import AllocatedProject from "./pages/allocations/AllocatedProject";
+import AddProject from "./pages/projects/AddProject";
+import EditProject from "./pages/projects/EditProject";
+import Project from "./pages/projects/Project";
+import Projects from "./pages/projects/Projects";
+import ProposedProjects from "./pages/proposals/ProposedProjects";
+import ShortlistedProjects from "./pages/shortlists/ShortlistedProjects";
+import { EditUser } from "./pages/users/EditUser";
+import User from "./pages/users/User";
 import AdminRoute from "./routes/AdminRoute";
 import AuthRoute from "./routes/AuthRoute";
 import GuestRoute from "./routes/GuestRoute";
 import StaffRoute from "./routes/StaffRoute";
 import StudentRoute from "./routes/StudentRoute";
 
-const router = createBrowserRouter([
-  {
-    path: "signin",
-    element: (
-      <CenterLayout>
-        <GuestRoute fallback="/">
-          <SignIn />
-        </GuestRoute>
-      </CenterLayout>
-    ),
-  },
-  {
-    element: (
-      <HeaderLayout>
-        <SiderLayout>
-          <AuthRoute fallback="/signin">
-            <Outlet />
-          </AuthRoute>
-        </SiderLayout>
-      </HeaderLayout>
-    ),
-    errorElement: (
-      <HeaderLayout>
-        <Error />
-      </HeaderLayout>
-    ),
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/projects" />,
-      },
-      {
-        path: "projects",
-        children: [
-          {
-            index: true,
-            element: <Projects />,
-            loader: projectsLoader,
-          },
-          {
-            path: ":id",
-            element: <Project />,
-            loader: projectLoader,
-          },
-          {
-            path: "add",
-            element: (
-              <StaffRoute fallback="/">
-                <AddProject />
-              </StaffRoute>
-            ),
-            action: addProjectAction,
-          },
-          {
-            path: ":id/edit",
-            element: (
-              <StaffRoute fallback="/">
-                <EditProject />
-              </StaffRoute>
-            ),
-            loader: editProjectLoader,
-            action: editProjectAction,
-          },
-        ],
-      },
-      {
-        path: "proposed",
-        element: (
-          <StaffRoute fallback="/">
-            <ProposedProject />
-          </StaffRoute>
-        ),
-        loader: proposedProjectLoader,
-      },
-      {
-        path: "shortlisted",
-        element: (
-          <StudentRoute fallback="/">
-            <ShortlistedProjects />
-          </StudentRoute>
-        ),
-        loader: shortlistedProjectsLoader,
-      },
-      {
-        path: "allocated",
-        element: (
-          <StudentRoute fallback="/">
-            <AllocatedProject />
-          </StudentRoute>
-        ),
-        loader: allocatedProjectLoader,
-      },
-      {
-        path: "admin",
-        element: (
-          <AdminRoute fallback="/">
-            <Outlet />
-          </AdminRoute>
-        ),
-        children: [
-          {
-            path: "users",
-            element: <ManageUsers />,
-            loader: manageUsersLoader,
-          },
-          {
-            path: "projects",
-            element: <ManageProjects />,
-            loader: manageProjectsLoader,
-          },
-          {
-            path: "allocations",
-            element: <ManageAllocations />,
-          },
-          {
-            path: "notifications",
-            element: <ManageNotifications />,
-          },
-          {
-            path: "data",
-            element: <ManageData />,
-          },
-        ],
-      },
-      {
-        path: "users",
-        children: [
-          {
-            path: ":id",
-            element: <User />,
-            loader: userLoader,
-          },
-          {
-            path: ":id/edit",
-            element: (
-              <AdminRoute fallback="/">
-                <EditUser />
-              </AdminRoute>
-            ),
-            loader: editUserLoader,
-            action: editUserAction,
-          },
-        ],
-      },
-    ],
-  },
-]);
+// We use createBrowserRouter() for data API support,
+// together with createRoutesFromElements() for readability.
+// https://reactrouter.com/en/main/routers/picking-a-router
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" errorElement={<Error />}>
+      <Route element={<GuestRoute fallback="/" />}>
+        <Route element={<CenterLayout />}>
+          <Route path="signin" element={<SignIn />} />
+        </Route>
+      </Route>
+      <Route element={<AuthRoute fallback="/signin" />}>
+        <Route element={<SiderLayout />}>
+          <Route index element={<Navigate to="/projects" />} />
+          <Route path="projects" handle={{ crumb: "All Projects" }}>
+            <Route index element={<Projects />} />
+            <Route path=":id" element={<Project />} handle={{ crumb: "Project" }} />
+            <Route element={<StaffRoute fallback="/" />}>
+              <Route path="add" element={<AddProject />} handle={{ crumb: "Add Project" }} />
+              <Route path=":id/edit" element={<EditProject />} handle={{ crumb: "Edit Project" }} />
+            </Route>
+          </Route>
+          <Route element={<StaffRoute fallback="/" />}>
+            <Route path="proposed" element={<ProposedProjects />} handle={{ crumb: "Proposed Projects" }} />
+          </Route>
+          <Route element={<StudentRoute fallback="/" />}>
+            <Route path="shortlisted" element={<ShortlistedProjects />} handle={{ crumb: "Shortlisted Projects" }} />
+            <Route path="allocated" element={<AllocatedProject />} handle={{ crumb: "Allocated Project" }} />
+          </Route>
+          <Route path="admins" element={<AdminRoute fallback="/" />} handle={{ crumb: "Admins" }}>
+            <Route path="users" element={<ManageUsers />} handle={{ crumb: "Manage Users" }} />
+            <Route path="projects" element={<ManageProjects />} handle={{ crumb: "Manage Projects" }} />
+            <Route path="allocations" element={<ManageAllocations />} handle={{ crumb: "Manage Allocations" }} />
+            <Route path="notifications" element={<ManageNotifications />} handle={{ crumb: "Manage Notifications" }} />
+            <Route path="data" element={<ManageData />} handle={{ crumb: "Manage Data" }} />
+          </Route>
+          <Route path="users" handle={{ crumb: "All Users" }}>
+            <Route index element={<Navigate to="/admins/users" />} />
+            <Route path=":id" element={<User />} handle={{ crumb: "User Profile" }} />
+            <Route element={<AdminRoute fallback="/" />}>
+              <Route path=":id/edit" element={<EditUser />} handle={{ crumb: "Edit User Profile" }} />
+            </Route>
+          </Route>
+        </Route>
+      </Route>
+    </Route>
+  )
+);
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MsalProvider instance={msalInstance}>
-      <ConfigContextProvider>
-        <TemplateContextProvider>
-          <UserContextProvider>
-            <MessageContextProvider>
-              <RouterProvider router={router} />
-            </MessageContextProvider>
-          </UserContextProvider>
-        </TemplateContextProvider>
-      </ConfigContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <MessageContextProvider>
+          <RouterProvider router={router} />
+        </MessageContextProvider>
+      </QueryClientProvider>
     </MsalProvider>
   </React.StrictMode>
 );
