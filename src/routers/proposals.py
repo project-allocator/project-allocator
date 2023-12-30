7b5fa0a184e8e8e1c0d8e3353726a471f1690d4c
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-12-30 10:38:39 +0900

    Refactor: Make read_proposed_projects return projects sorted by update datetime

--- src/routers/proposals.py
+++ src/routers/proposals.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, Security
+from fastapi import APIRouter, HTTPException, Depends, Security
 from sqlmodel import Session
 
 from ..dependencies import check_staff, get_session, get_user
@@ -13,8 +13,12 @@ router = APIRouter(tags=["proposal"])
     response_model=list[ProjectRead],
     dependencies=[Security(check_staff)],
 )
-async def read_proposed(user: Annotated[User, Depends(get_user)]):
-    return [proposal.proposed_project for proposal in user.proposals]
+async def read_proposed_projects(user: Annotated[User, Depends(get_user)]):
+    # Sort the projects so that the last updated project is returned first.
+    proposed_projects = [proposal.proposed_project for proposal in user.proposals]
+    proposed_projects.sort(key=lambda project: project.updated_at, reverse=True)
+
+    return proposed_projects
 
 
 @router.get(
