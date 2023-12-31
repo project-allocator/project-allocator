9e17a86279ead6203b8635a2bacba0906d559aef
Taichi Maeda <taichi.maeda.up@gmail.com>
2023-12-31 20:15:32 +0900

    Refactor: Rewrite users pages using Await component

--- src/pages/users/EditUser.tsx
+++ src/pages/users/EditUser.tsx
@@ -1,5 +1,6 @@
+import { useMessage } from "@/contexts/MessageContext";
 import { useUpdateUser, useUser } from "@/hooks/users";
-import Loading from "@/pages/Loading";
+import Await from "@/pages/Await";
 import { Button, Divider, Form, Input, Select, Typography } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -7,51 +8,59 @@ const { Title } = Typography;
 
 export function EditUser() {
   const navigate = useNavigate();
+  const { messageError, messageSuccess } = useMessage();
 
   const { id: userId } = useParams();
   const user = useUser(userId!);
   const updateUser = useUpdateUser(userId!);
 
-  if (user.isLoading) return <Loading />;
-  if (user.isError) return null;
+  if (user.isLoading || user.isError) return null;
 
   return (
     <>
       <Title level={3}>Edit User Profile</Title>
       <Divider />
-      <Form
-        method="post"
-        layout="vertical"
-        autoComplete="off"
-        className="ml-6 max-w-xl"
-        onFinish={(values) =>
-          updateUser.mutate(values, {
-            onSuccess: () => navigate(-1),
-          })
-        }
-      >
-        <Form.Item label="Name" name="name" initialValue={user.data!.name}>
-          <Input disabled />
-        </Form.Item>
-        <Form.Item label="Email" name="email" initialValue={user.data!.email}>
-          <Input disabled />
-        </Form.Item>
-        <Form.Item label="Role" name="role" initialValue={user.data!.role}>
-          <Select
-            className="w-48"
-            options={[
-              { value: "admin", label: "Admin" },
-              { value: "staff", label: "Staff" },
-              { value: "student", label: "Student" },
-            ]}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <Await query={user} errorElement="Failed to load user">
+        {(user) => (
+          <Form
+            method="post"
+            layout="vertical"
+            autoComplete="off"
+            className="ml-6 max-w-xl"
+            onFinish={(values) =>
+              updateUser.mutate(values, {
+                onSuccess: () => {
+                  messageSuccess("Successfully updated user");
+                  navigate(-1);
+                },
+                onError: () => messageError("Failed to update user"),
+              })
+            }
+          >
+            <Form.Item label="Name" name="name" initialValue={user.name}>
+              <Input disabled />
+            </Form.Item>
+            <Form.Item label="Email" name="email" initialValue={user.email}>
+              <Input disabled />
+            </Form.Item>
+            <Form.Item label="Role" name="role" initialValue={user.role}>
+              <Select
+                className="w-48"
+                options={[
+                  { value: "admin", label: "Admin" },
+                  { value: "staff", label: "Staff" },
+                  { value: "student", label: "Student" },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        )}
+      </Await>
     </>
   );
 }
--- src/pages/users/User.tsx
+++ src/pages/users/User.tsx
@@ -1,6 +1,6 @@
 import UserEditDeleteButtons from "@/components/users/UserEditDeleteButtons";
 import { useCurrentUserRole, useUser } from "@/hooks/users";
-import Loading from "@/pages/Loading";
+import Await from "@/pages/Await";
 import { toCapitalCase } from "@/utils";
 import { Divider, Space, Typography } from "antd";
 import { useParams } from "react-router-dom";
@@ -10,10 +10,8 @@ const { Title, Paragraph } = Typography;
 export default function User() {
   const { id: userId } = useParams();
   const user = useUser(userId!);
-  const { isAdmin } = useCurrentUserRole();
 
-  if (user.isLoading) return <Loading />;
-  if (user.isError) return null;
+  const { isAdmin } = useCurrentUserRole();
 
   return (
     <>
@@ -24,19 +22,27 @@ export default function User() {
         {isAdmin && <UserEditDeleteButtons />}
       </Space>
       <Divider />
-      <Title level={4}>Name</Title>
-      <Paragraph className="text-slate-500">
-        Full name of the user, which is based on the user's profile on Microsoft.
-      </Paragraph>
-      <Paragraph>{user.data!.name}</Paragraph>
-      <Title level={4}>Email Address</Title>
-      <Paragraph className="text-slate-500">
-        Email address of the user, which is based on the user's profile on Microsoft.
-      </Paragraph>
-      <Paragraph>{user.data!.email}</Paragraph>
-      <Title level={4}>Role</Title>
-      <Paragraph className="text-slate-500">Role of the user. This will be one of student, staff or admin.</Paragraph>
-      <Paragraph>{toCapitalCase(user.data!.role)}</Paragraph>
+      <Await query={user} errorElement="Failed to load user">
+        {(user) => (
+          <>
+            <Title level={4}>Name</Title>
+            <Paragraph className="text-slate-500">
+              Full name of the user, which is based on the user's profile on Microsoft.
+            </Paragraph>
+            <Paragraph>{user.name}</Paragraph>
+            <Title level={4}>Email Address</Title>
+            <Paragraph className="text-slate-500">
+              Email address of the user, which is based on the user's profile on Microsoft.
+            </Paragraph>
+            <Paragraph>{user.email}</Paragraph>
+            <Title level={4}>Role</Title>
+            <Paragraph className="text-slate-500">
+              Role of the user. This will be one of student, staff or admin.
+            </Paragraph>
+            <Paragraph>{toCapitalCase(user.role)}</Paragraph>
+          </>
+        )}
+      </Await>
     </>
   );
 }
