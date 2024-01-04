cc98c5120a19fa4cae9de76f019658e015f82455
Taichi Maeda <taichi.maeda.up@gmail.com>
2024-01-04 13:27:33 +0900

    Feat: Show proposer name in ProjectTable component

--- frontend/src/components/projects/ProjectTable.tsx
+++ frontend/src/components/projects/ProjectTable.tsx
@@ -1,5 +1,6 @@
-import { ProjectRead } from "@/api";
+import { ProjectRead, ProjectReadWithProposal } from "@/api";
 import { usePrefetchProject } from "@/hooks/projects";
+import { usePrefetchUser } from "@/hooks/users";
 import { Input, Table } from "antd";
 import { useState } from "react";
 import Highlighter from "react-highlight-words";
@@ -7,16 +8,17 @@ import { Link } from "react-router-dom";
 
 const { Search } = Input;
 
-export function ProjectTable({ projects }: { projects: ProjectRead[] }) {
+export function ProjectTable({ projects }: { projects: ProjectReadWithProposal[] }) {
   const [searchText, setSearchText] = useState("");
 
-  // Prefetch project data when hovering over a project item
+  // Prefetch project and proposer data when hovering over an item
   const prefetchProject = usePrefetchProject();
+  const prefetchUser = usePrefetchUser();
 
   const columns = [
     {
       title: "Title",
-      render: (project: ProjectRead) => (
+      render: (project: ProjectReadWithProposal) => (
         <Link to={`/projects/${project.id}`} onMouseOver={() => prefetchProject(project.id)}>
           <Highlighter searchWords={[searchText]} textToHighlight={project.title} />
         </Link>
@@ -24,13 +26,24 @@ export function ProjectTable({ projects }: { projects: ProjectRead[] }) {
     },
     {
       title: "Description",
-      render: (project: ProjectRead) => (
+      render: (project: ProjectReadWithProposal) => (
         <Highlighter
           searchWords={[searchText]}
           textToHighlight={
             project.description.length < 500 ? project.description : project.description.slice(0, 500) + "..."
           }
         />
+      ),
+    },
+    {
+      title: "Proposer",
+      render: (project: ProjectReadWithProposal) => (
+        <Link
+          to={`/users/${project.proposal.proposer.id}`}
+          onMouseOver={() => prefetchUser(project.proposal.proposer.id)}
+        >
+          <Highlighter searchWords={[searchText]} textToHighlight={project.proposal.proposer.name} />
+        </Link>
       ),
     },
   ];
@@ -48,7 +61,11 @@ export function ProjectTable({ projects }: { projects: ProjectRead[] }) {
       <Table
         columns={columns}
         dataSource={projects
-          .filter((project) => (project.title + project.description).toLowerCase().includes(searchText.toLowerCase()))
+          .filter((project) =>
+            (project.title + project.description + project.proposal.proposer.name)
+              .toLowerCase()
+              .includes(searchText.toLowerCase())
+          )
           .map((project: ProjectRead) => ({ key: project.id, ...project }))}
       />
     </>
