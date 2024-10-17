4520d2a3b9fb63e2ce2492019b800968e473b037
Ivan Procaccini <ivanprocaccini905@gmail.com>
2024-10-17 13:44:51 +0100

    refactor: revise db url building routine

--- backend/src/db.py
+++ backend/src/db.py
@@ -2,12 +2,14 @@ from sqlmodel import create_engine
 
 # Must be imported beforehand to setup SQLModel.metadata
 from . import models
-from .env import DATABASE_DOMAIN, DATABASE_PASSWORD, DATABASE_SSL, DATABASE_USERNAME, FASTAPI_ENV
+from .env import (
+    FASTAPI_ENV,
+    DATABASE_USERNAME,
+    DATABASE_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_NAME,
+)
 
-# Azure PostgreSQL requires the username to be in the format of 'username@domain'
-if FASTAPI_ENV == "production":
-    DATABASE_USERNAME = f"{DATABASE_USERNAME}@{DATABASE_DOMAIN}"
-
-DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_DOMAIN}:5432/default{'?sslmode=require' if DATABASE_SSL == 'true' else ''}"
-
+DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 engine = create_engine(DATABASE_URL)
